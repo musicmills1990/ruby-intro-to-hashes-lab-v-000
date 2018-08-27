7ebd7fe75df238e47980e521623b6772f48ec4fd
Marc Mills <mdmills1990@gmail.com>
2018-08-27 17:05:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,9 +30,9 @@ def monopoly_with_third_tier
 		:pieces => 4,
 		:names => {
 			:reading_railroad => {},
-			:shortline => {},
 			:pennsylvania_railroad => {},
-			:b_and_o_railroad => {}
+			:b_and_o_railroad => {},
+			:shortline => {}
 			},
 		:rent_in_dollars => {
 			:one_piece_owned => 25,
